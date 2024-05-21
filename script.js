e34d436d4d9c58eb556e2c632f67c54232f933a6
zxHollow <dracotemporum@gmail.com>
2024-05-20 22:14:47 -0300

    "

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu(){
+    if(mn.style.display == "none") { //verifica se a imagem está sendo exibida, se não estiver vai executar os comandos abaixo e se tiver vai executar o else
+      mn.style.display="block"
+      ul.style.display="block";
+
+    }
+    else { //se a imagem estiver sendo exibida vai executar os comandos abaixo
+      mn.style.display="none";
+      ul.style.display="none";
+    }
+   }
